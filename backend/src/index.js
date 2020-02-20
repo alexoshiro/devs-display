0bf286c2f0704ced2a22b3dff2126b67a592c5ed
Alex Oshiro <alex.oshiro@e-deploy.com.br>
2020-02-19 20:19:42 -0400

    Added Cors. Wraped dotenv init in try catch block.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
-require('dotenv').config();
 
-const app = express();
+try {
+    require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-});
+    const app = express();
 
-app.use(express.json());
-app.use(routes);
-app.listen(process.env.API_PORT);
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    });
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(routes);
+    app.listen(process.env.API_PORT);
+} catch (err) {
+    console.log(err);
+}
