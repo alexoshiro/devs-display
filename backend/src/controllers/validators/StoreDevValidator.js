8930cec2f1589f9c70e34dd8d6aded001e24bad6
Alex Oshiro <alex.oshiro@e-deploy.com.br>
2020-02-20 13:25:00 -0400

    Custom message response from validations.

--- backend/src/controllers/validators/StoreDevValidator.js
+++ backend/src/controllers/validators/StoreDevValidator.js
@@ -1,10 +1,29 @@
 const Joi = require('@hapi/joi');
 
 const storeSchema = Joi.object().keys({
-  github_username: Joi.string().alphanum().required(),
-  techs: Joi.string().required(),
-  latitude: Joi.number().required(),
+  github_username: Joi.string().alphanum().required()
+    .messages({
+      'string.alphanum': 'Valor de usuário do github deve conter apenas alfanumérico.',
+      'string.base': 'Valor de usuário do github deve ser um texto',
+      'string.empty': 'Valor de usuário do github não pode ser vazio.',
+      'any.required': 'Valor de usuário do github é obrigatório.'
+    }),
+  techs: Joi.string().required()
+    .messages({
+      'string.base': 'Valor de tecnologias deve ser um texto',
+      'string.empty': 'Valor de tecnologias não pode ser vazio.',
+      'any.required': 'Valor de tecnologias é obrigatório.'
+    }),
+  latitude: Joi.number().required()
+    .messages({
+      'number.base': 'Valor de latitude deve ser um número.',
+      'any.required': 'Valor de latitude é obrigatório.'
+    }),
   longitude: Joi.number().required()
+    .messages({
+      'number.base': 'Valor de longitude deve ser um número.',
+      'any.required': 'Valor de longitude é obrigatório.'
+    })
 });
 
 module.exports = storeSchema;
--- backend/src/controllers/validators/UpdateDevValidator.js
+++ backend/src/controllers/validators/UpdateDevValidator.js
@@ -1,9 +1,18 @@
 const Joi = require('@hapi/joi');
 
 const updateSchema = Joi.object().keys({
-  techs: Joi.string(),
-  latitude: Joi.number(),
+  techs: Joi.string()
+    .messages({
+      'string.base': 'Valor de usuário do github deve ser um texto'
+    }),
+  latitude: Joi.number()
+    .messages({
+      'number.base': 'Valor de latitude deve ser um número.'
+    }),
   longitude: Joi.number()
+    .messages({
+      'number.base': 'Valor de latitude deve ser um número.'
+    })
 })
   .with('latitude', 'longitude')
   .with('longitude', 'latitude');
